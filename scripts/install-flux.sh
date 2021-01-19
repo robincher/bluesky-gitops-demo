a7064f86f7dffaad12ebad05172094654b633a00
Robin Cher <robincher@gmail.com>
2021-01-19 18:21:04 +0800

    Initial commit for BlueSky app

--- scripts/install-flux.sh
+++ scripts/install-flux.sh
@@ -3,10 +3,11 @@ kubectl create ns flux
 
 # Run Flux setup
 export GHUSER="robincher"
+export REPOSITORY="bluesky-gitops-demo"
 fluxctl install \
 --git-user=${GHUSER} \
 --git-email=${GHUSER}@users.noreply.github.com \
---git-url=git@github.com:${GHUSER}/gitops-demo \
+--git-url=git@github.com:${GHUSER}/${REPOSITORY} \
 --git-branch=master \
 --git-path=workloads \
 --namespace=flux | kubectl apply -f -
