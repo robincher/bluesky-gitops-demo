4f55e3fa287e6e68f517799a3741febd6581ec99
Robin Cher <robincher@users.noreply.github.com>
2021-05-20 19:35:24 +0800

    Update

--- scripts/install-flux.sh
+++ scripts/install-flux.sh
@@ -1,6 +1,14 @@
 # Create Flux NS
 kubectl create ns flux
 
+# Script to pull flux-git-deploy secrets from AWS Secret Manager
+#FLUX_GIT_SECRET=$( aws secretsmanager get-secret-value --secret-id platform/kubernetes/secret/flux/git-deploy-key  --query SecretString --output text)
+#echo "$FLUX_GIT_SECRET" > ~/tmp/flux-secret.yaml
+
+# Set-up Flux and memcache
+#kubectl apply -f ~/tmp/flux-secret.yaml -n flux
+
+
 # Run Flux setup
 export GHUSER="robincher"
 export REPOSITORY="bluesky-gitops-demo"
